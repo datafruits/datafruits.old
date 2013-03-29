9d665123f6cf328b426e3f1a7baa05c92ad5e167
Tony Miller <mcfiredrill@gmail.com>
2013-03-28 18:33:26 -0700

    woops forgot vcr stuff and fixture

---
+++ features/support/vcr.rb
@@ -0,0 +1,11 @@
+require 'vcr'
+
+VCR.configure do |c|
+  c.hook_into :webmock
+  c.cassette_library_dir     = 'features/cassettes'
+  c.default_cassette_options = { :record => :new_episodes }
+end
+
+VCR.cucumber_tags do |t|
+  t.tag '@vcr'
+end
