5359497aefafb4e8dbc361fa26dcd8adcccc2fd8
Tony Miller <mcfiredrill@gmail.com>
2013-03-31 21:50:00 -0700

    fix sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,26 @@
+# the worst case of cargo culting i have ever commited :(
+
 class SessionsController < Devise::SessionsController
   def create
-    logger.info "CREATE" 
-    resource = warden.authenticate!(:scope => resource_name, :recall => :failure)
-    return sign_in_and_redirect(resource_name, resource)
+    respond_to do |format|
+      format.html { super }
+      format.json {
+        resource = warden.authenticate!(:scope => resource_name, :recall => :failure)
+        return sign_in_and_redirect(resource_name, resource)
+      }
+    end
   end
   
   def sign_in_and_redirect(resource_or_scope, resource=nil)
     scope = Devise::Mapping.find_scope!(resource_or_scope)
     resource ||= resource_or_scope
     sign_in(scope, resource) unless warden.user(scope) == resource
-    logger.info "signed in"
     respond_to do |format|
       format.html {
         set_flash_message(:notice, :signed_in) if is_navigational_format?
         respond_with resource, :location => after_sign_in_path_for(resource)
       }
       format.json {
-        logger.info "about to render json"
         render :json => {:success => true, :redirect => stored_location_for(scope) || after_sign_in_path_for(resource)}
       }
     end
