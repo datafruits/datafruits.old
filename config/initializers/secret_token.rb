820c7f0c1c028bc169da75beff8072dc0fed72fa
Tony Miller <mcfiredrill@gmail.com>
2013-06-01 05:52:52 -0700

    stop storing the secret token! X(

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DatafruitsRails::Application.config.secret_token = 'e69895ab06b9ca7e910bbf47a4af7f93b80786c07d3b29605da8d485d7689ac79431b773d431b9d74ce8c9a059667271a4aa1a5091bdf0304b30281d7487f87e'
+DatafruitsRails::Application.config.secret_token = ENV['SECRET_TOKEN']
