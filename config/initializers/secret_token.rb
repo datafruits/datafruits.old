cb859c311746fe85b0a395d435f0846f0b85cbfa
Tony Miller <mcfiredrill@gmail.com>
2013-06-11 17:11:05 -0700

    use stored secret token on non-prod envrionments

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DatafruitsRails::Application.config.secret_token = ENV['SECRET_TOKEN']
+if ENV['RAILS_ENV'] == 'production'
+  DatafruitsRails::Application.config.secret_token = ENV['SECRET_TOKEN']
+else
+  DatafruitsRails::Application.config.secret_token = '9f642a303ffebfe934c9534af35af22e9c704484a2d33aec7b8cb07e4f88ce1f8f9c7fc46d03413f2ab002e06c85d0497f450d0587276117a1ef5555e167be45'
+end
