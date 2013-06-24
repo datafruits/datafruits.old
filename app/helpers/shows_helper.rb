bac1607b013e7bb7d627e13b4a77e92e425104ea
Tony Miller <mcfiredrill@gmail.com>
2013-06-23 17:13:17 -0700

    link to show

--- app/helpers/shows_helper.rb
+++ app/helpers/shows_helper.rb
@@ -1,2 +1,9 @@
 module ShowsHelper
+  def link_to_show(show)
+    if !show.title.blank?
+      link_to "#{show.title} - #{show.time.strftime("%A %H:%M %Z")}", show 
+    else
+      link_to "#{show.user.username} - #{show.time.strftime("%A %H:%M %Z")}", show
+    end
+  end
 end
