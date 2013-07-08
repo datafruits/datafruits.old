5dabbc0ed867729f011c5fa4092852c2fc846f56
Tony Miller <mcfiredrill@gmail.com>
2013-07-07 18:00:43 -0700

    show date for show instead of just day of week

--- app/helpers/shows_helper.rb
+++ app/helpers/shows_helper.rb
@@ -1,9 +1,12 @@
 module ShowsHelper
   def link_to_show(show)
     if !show.title.blank?
-      link_to "#{show.title} - #{show.time.strftime("%A %H:%M %Z")}", show 
+      link_to "#{show.title}", show 
     else
       link_to "#{show.user.username} - #{show.time.strftime("%A %H:%M %Z")}", show
     end
+  end
+  def link_to_show_with_time(show)
+    link_to "#{show.time.strftime("%A %m/%d %H:%M %Z")}", show 
   end
 end
