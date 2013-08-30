74fc5f5d4a505c1652c068f2bed8f943bed7c903
Tony Miller <mcfiredrill@gmail.com>
2013-08-29 21:59:16 -0700

    set file path for podcasts

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -6,5 +6,19 @@ class Podcast < ActiveRecord::Base
       access_key_id: ENV['S3_KEY'],
       secret_access_key: ENV['S3_SECRET']
     },
-    bucket: 'datafruits.fm'
+    bucket: 'datafruits.fm',
+    path: '/:filename',
+    preserve_files: true
+
+  def set_file_as_attachment file
+    s3 = AWS::S3.new
+    obj = s3.buckets['datafruits.fm'].objects["#{file}"]
+    puts obj
+    self.mp3_file_name    = file
+    self.mp3_content_type = MIME::Types.type_for(self.mp3_file_name).first.to_s
+    #self.mp3_file_size    = obj.content_length
+    self.mp3_updated_at   = Time.now
+    puts "saving"
+    self.save!
+  end
 end
