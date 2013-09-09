d9071449dae0a9d69728634ae62e599a6cf3240f
Tony Miller <mcfiredrill@gmail.com>
2013-09-08 21:53:51 -0700

    change tweet text

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,19 @@ module ApplicationHelper
   def next_scheduled_show
     show = Show.where("time >= ?", Time.current).order("time ASC").first
   end
+
+  def multiple_timezones(time)
+  timezones = {"PST" => "Pacific Time (US & Canada)",
+               "EST" => "Eastern Time (US & Canada)",
+               "UK"  => "London",
+               "EU"  => "Stockholm",
+               "日本"=> "Tokyo"}
+  time_string = ""
+
+  timezones.each do |k,v|
+    time_string << "[#{Time.zone.parse(time.to_s).in_time_zone(v).strftime("%H:%M")} #{k}] "
+  end
+
+  time_string
+  end
 end
--- app/helpers/shows_helper.rb
+++ app/helpers/shows_helper.rb
@@ -9,4 +9,8 @@ module ShowsHelper
   def link_to_show_with_time(show)
     link_to "#{show.time.strftime("%A %m/%d %H:%M %Z")}", show 
   end
+
+  def tweet_text(show)
+    "#{show.user.username} - #{show.title} on @datafruits #{show.time.strftime("%m/%d")} - #{multiple_timezones(show.time)}"
+  end
 end
