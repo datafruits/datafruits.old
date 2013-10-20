423c33f04248dbb122d89d715347a62364ffaba4
Tony Miller <mcfiredrill@gmail.com>
2013-10-20 21:52:11 +0900

    now we have stars ok

--- app/assets/javascripts/data_dayz.js
+++
@@ -1,9 +0,0 @@
-//= require jquery
-
-$(document).ready(function(){
-  for(var i = 0; i < 200; i++){
-    var el = $("<span id='scroller'>★</span>");
-    $("#scroll-container").append(el);
-    el.css('left',i*50);
-  }
-});
