dc0a87dd5c07958cce2c7825c898f837fb9dfccc
Tony Miller <mcfiredrill@gmail.com>
2013-12-04 08:31:53 +0900

    mobile filters in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :prepare_for_mobile
 
   around_filter :set_time_zone
 
@@ -67,6 +68,19 @@ class ApplicationController < ActionController::Base
   end
 
   private
+
+  def mobile_device?
+    if session[:mobile_override]
+      session[:mobile_override] == "1"
+    else
+      (request.user_agent =~ /Mobile|webOS/) && (request.user_agent !~ /iPad/)
+    end
+  end
+  helper_method :mobile_device?
+
+  def prepare_for_mobile
+    session[:mobile_override] = params[:mobile] if params[:mobile]
+  end
 
   def set_locale
     I18n.locale = params[:locale] || extract_locale_from_accept_language_header || I18n.default_locale
