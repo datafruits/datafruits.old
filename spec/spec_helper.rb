e8082a5e43ecc5e8c508836403fe7d38d69e5dc0
Tony Miller <mcfiredrill@gmail.com>
2013-12-20 16:06:35 +0900

    try no './'

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,7 +39,7 @@ RSpec.configure do |config|
 end
 
 VCR.configure do |c|
-  c.cassette_library_dir = './spec/fixtures/vcr_cassettes'
+  c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   c.hook_into :webmock
   c.default_cassette_options = { :record => :new_episodes }
 end
