a85ba5665bd942be16b2babed2ccc088ce432214
Tony Miller <mcfiredrill@gmail.com>
2014-04-18 12:14:57 -0700

    fix shows controller

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,48 +1,31 @@
 class ShowsController < ApplicationController
   load_and_authorize_resource
-  # GET /shows
-  # GET /shows.json
-  def index
-    #@shows = Show.all
 
+  def index
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @shows }
     end
   end
 
-  # GET /shows/1
-  # GET /shows/1.json
   def show
-    #@show = Show.find(params[:id])
-
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @show }
     end
   end
 
-  # GET /shows/new
-  # GET /shows/new.json
   def new
-    #@show = Show.new
-
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @show }
     end
   end
 
-  # GET /shows/1/edit
   def edit
-    #@show = Show.find(params[:id])
   end
 
-  # POST /shows
-  # POST /shows.json
   def create
-    #@show = Show.new(params[:show])
-
     respond_to do |format|
       if @show.save
         format.html { redirect_to @show, notice: 'Show was successfully created.' }
@@ -54,13 +37,9 @@ class ShowsController < ApplicationController
     end
   end
 
-  # PUT /shows/1
-  # PUT /shows/1.json
   def update
-    #@show = Show.find(params[:id])
-
     respond_to do |format|
-      if @show.update_attributes(params[:show])
+      if @show.update_attributes(show_params)
         format.html { redirect_to @show, notice: 'Show was successfully updated.' }
         format.json { head :no_content }
       else
@@ -70,10 +49,7 @@ class ShowsController < ApplicationController
     end
   end
 
-  # DELETE /shows/1
-  # DELETE /shows/1.json
   def destroy
-    #@show = Show.find(params[:id])
     @show.destroy
 
     respond_to do |format|
